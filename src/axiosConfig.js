f9a2390345f025a971963f7f5382d259b5974f65
Harsh Vardhan Gupta <harsh247@gmail.com>
2024-09-25 15:49:17 +0530

    Debuggeg axios config file, now it dynamically gets token with every request

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,16 +1,29 @@
 import axios from 'axios';
 
+
 // Create an instance of axios with a custom config
+// For development, use 'http://localhost:5000/api' as your backend API endpoint.
 const axiosInstance = axios.create({
   baseURL: 'https://c2-backend-plv7.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
   timeout: 5000,
   headers: {
-    'Authorization': `Bearer ${localStorage.getItem('token')}`,
     'Content-Type': 'application/json',
   },
-  // You can add more default settings here, for example:
-  // timeout: 1000,
-  // headers: {'X-Custom-Header': 'foobar'}
 });
+
+// Add a request interceptor to dynamically get the token from the local storage
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); // Dynamically get the token
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`; // Attach token to Authorization header
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
